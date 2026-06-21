a028d3fe62cb42c83588ab989377617cde401f3c
Pader <ypnow@163.com>
2026-06-21 16:02:57 +0800

    Add GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/cube-solver/' : '/',
   server: {
     open: true,
   },
-});
+}));
